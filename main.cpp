65674a0d3178652f86fd37378b74f82d2bdcde3f
ABulger01 <Nyesha2001@gmail.com>
2025-03-28 18:58:52 +0000

    added if/else statements for range of months

--- main.cpp
+++ main.cpp
@@ -3,5 +3,10 @@
 int main () {
   int month, year;
 
-  
+  std::cout << "Enter a month (1-12): ";
+  if (month < 1 || month > 12)
+    cout << "Invalid month, please enter a number between 1 and 12." << endl;
+return 1
+  std::cin >> month;
+  std::cout << "Enter a year: ";
 }
