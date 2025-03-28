0b82aef0c902269ac711d476bad2e0f927b6de59
ABulger01 <Nyesha2001@gmail.com>
2025-03-28 20:49:22 +0000

    added default days and finishing sentence

--- main.cpp
+++ main.cpp
@@ -21,7 +21,12 @@ return 1;
   // Determine the number of days in the month
   if ( month == 2 )
     days = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 29: 28;
-    
+  else if ( month == 4 || month == 6 || month == 9 || month == 11 )
+    days = 30;
+  else
+    days = 31;
+  std::cout << "The month has " << days << " days." << std::endl;
+  
     
     
   
