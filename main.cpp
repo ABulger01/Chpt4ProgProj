8dd3d922b03259438e1ba4ea40e78ba0dffeade7
ABulger01 <Nyesha2001@gmail.com>
2025-03-28 20:37:25 +0000

    added leap year equation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
 int main () {
-  int month, year;
+  int month;
 
   std::cout << "Enter a month (1-12): ";
-  if (month < 1 || month > 12)
-    cout << "Invalid month, please enter a number between 1 and 12." << endl;
-return 1
   std::cin >> month;
+  if ( month < 1 || month > 12 )
+    std::cout << "Invalid month! Please enter a number between 1 and 12." << std::endl;
+return 1;
+  int year;
   std::cout << "Enter a year: ";
+  std::cin >> year;
+  // Determine if the year is a leap year
+  if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 )
+    std::cout << year << " is a leap year. February has 29 days." << std::endl;
+  else
+    std::cout << year << " is not a leap year. "
 }
