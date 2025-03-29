371caad80992beabe473ebe08df339ebd87321ff
ABulger01 <Nyesha2001@gmail.com>
2025-03-29 23:09:21 +0000

    getting the variables for the distance

--- main.cpp
+++ main.cpp
@@ -10,7 +10,11 @@ int main() {
   cout << "Enter the weight of the package: ";
   cin >> weight;
   if ( weight <= 0 || weight > 20) {
+    cout << "Invalid weight, please enter a weight between 0 and 20.";
+    return 1;
     
   }
-  // Get user input for weight
-  cout << "Enter the "
+  // Get user input for distance to be shipped
+  cout << "Enter the distance to be shipped:";
+  cin >> distance;
+  
