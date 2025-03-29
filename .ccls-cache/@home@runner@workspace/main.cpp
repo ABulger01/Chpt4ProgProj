dc874c486a3632ce86f32e6dabf9f8a6e6630f9c
ABulger01 <Nyesha2001@gmail.com>
2025-03-29 23:05:38 +0000

    entered the range for the weight of the packages

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -7,6 +7,10 @@ int main() {
   int distance; 
   double rate;
   double total;
-  cout << "Enter the w"
+  cout << "Enter the weight of the package: ";
+  cin >> weight;
+  if ( weight <= 0 || weight > 20) {
+    
+  }
   // Get user input for weight
   cout << "Enter the "
