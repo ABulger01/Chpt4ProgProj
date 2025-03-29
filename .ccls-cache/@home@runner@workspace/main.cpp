f196b68ef3e7d5289cbadd4bffdc6d8211cb26fc
ABulger01 <Nyesha2001@gmail.com>
2025-03-29 23:14:06 +0000

    adding constant weights

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -17,4 +17,13 @@ int main() {
   // Get user input for distance to be shipped
   cout << "Enter the distance to be shipped:";
   cin >> distance;
+  if (distance < 10 || distance > 3000) 
+    cout << "Invalid distance, please enter a distance between 10 and 3000.";
+  return 1;
+  // Calculate the rate based on weight
+  double rate;
+  if (weight <= 2)
+    rate = 1.10;
+  else if ( weight <= 6)
+      
   
