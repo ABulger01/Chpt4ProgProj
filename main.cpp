205e1ccd0179e47887c2499c415b99c408ca4f49
ABulger01 <Nyesha2001@gmail.com>
2025-03-30 01:48:06 +0000

    choices added

--- main.cpp
+++ main.cpp
@@ -6,5 +6,9 @@ std::cout << "Geometry Calculator" << std::endl;
  int choice;
   std::cout << "1. Calculate the Area of a Circle" << std::endl;
   std::cout << "2. Calculate the Area of a Rectangle" << std::endl;
-  std::cout << 
+  std::cout << "3. Calculate the Area of a Triangle" << std::endl;
+  std::cout << "4. Quit" << std::endl;
+  std::cout << "Ener your choice (1-4): ";
+  std::cin << choice;
+  
   
