af3f058c6ef5bb26058222bd4e664d0f2692bb9a
ABulger01 <Nyesha2001@gmail.com>
2025-03-30 01:58:54 +0000

    added all choice for each shape

--- main.cpp
+++ main.cpp
@@ -9,6 +9,24 @@ std::cout << "Geometry Calculator" << std::endl;
   std::cout << "3. Calculate the Area of a Triangle" << std::endl;
   std::cout << "4. Quit" << std::endl;
   std::cout << "Ener your choice (1-4): ";
-  std::cin << choice;
-  
+  std::cin >> choice;
+  if (choice == 1)
+    
+    
+switch (choice) {
+  case 1:
+  std::cout << "Enter the radius of the circle: ";
+    break;
+  case 2:
+    std::cout << "Enter the length and width of the rectangle: ";
+    break;
+    case 3:
+      std::cout << "Enter the base and height of the triangle: "
+    
+  if (radius < 0) {
+    std::cout << "Error: Radius cannot be negative." << std::endl;
+    
+  }
+    
+}
   
